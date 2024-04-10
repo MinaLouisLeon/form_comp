664ec21a17c8ee0ef41279c6eb70171debf46cf2
MinaLouisLeon <31774352+MinaLouisLeon@users.noreply.github.com>
2024-04-10 08:56:54 +0200

    Update getData.ts

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,6 +1,6 @@
 export async function getData(url: string) {
     //@ts-ignore
-    let privateKey:string  = process.env.NEXT_PUBLIC_API_KEY;
+    let privateKey:string  = process.env.API_KEY;
     try {
         const res = await fetch(url, {
             method: "GET",
